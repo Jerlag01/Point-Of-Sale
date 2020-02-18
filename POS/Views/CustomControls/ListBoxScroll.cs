af6ee891f9ed7982097b40d94a21df31a3941445
Lagaet <jerlag_01@outlook.com>
2020-02-18 08:38:40 +0100

    Modifications
    Changes file names in Views.
    Adds xaml files in views aswell as some functionality dependencies

---
+++ POS/Views/CustomControls/ListBoxScroll.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace Pos.Views.CustomControls
+{
+    internal class ListBoxScroll : ListBox
+    {
+        public ListBoxScroll()
+        {
+            this.SelectionChanged += new SelectionChangedEventHandler(this.ListBoxScroll_SelectionChanged);
+        }
+
+        private void ListBoxScroll_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            this.ScrollIntoView(this.SelectedItem);
+        }
+    }
+}
